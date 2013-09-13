1a3b664ad014cada36b0a7288eedf197b3270d79
Kozhuhov Andrey <kozhuhov.andrey@gmail.com>
2013-09-13 19:26:13 +0700

    Spaces fixed

--- RecentOrders_Api_Endpoint.php
+++ RecentOrders_Api_Endpoint.php
@@ -8,36 +8,35 @@ Author: Andrey Kozhuhov
 Author URL: http://www.cimpleo.com
 */
 
-class RecentOrders_API_Endpoint{
+class RecentOrders_API_Endpoint {
 	
-	public function __construct(){
+	public function __construct() {
 		add_filter( 'query_vars', array( $this, 'add_query_vars' ), 0 );
 		add_action( 'parse_request', array( $this, 'parse' ), 0 );
 		add_action( 'init', array( $this, 'add_endpoint' ), 0 );
 	}	
 
-	public function add_query_vars( $vars ){
+	public function add_query_vars( $vars ) {
 		$vars[] = 'wc_recent_orders';
 		return $vars;
 	}
 
-	public function parse(){
+	public function parse() {
 		global $wp;
-		if( isset( $wp->query_vars['wc_recent_orders'] ) ){
+		if( isset( $wp->query_vars['wc_recent_orders'] ) ) {
 			$this->handle();
 			exit;
 		}
 	}
 	
-	protected function handle(){
+	protected function handle() {
 		global $wp;
 		$count = $wp->query_vars['wc_recent_orders'];
 		$count || $count = get_option( 'posts_per_page' );
 		$this->response( '200 OK', $this->getRecentOrders( $count ) );
 	}
 	
-	protected function getRecentOrders( $count )
-	{		
+	protected function getRecentOrders( $count ) {		
 		$args = array(
 		'numberposts' => $count,
 		'orderby' => 'post_date',
@@ -50,11 +49,10 @@ class RecentOrders_API_Endpoint{
 		foreach ( $recent_posts as $post ) {
 			$orders[] = new WC_Order( $post['ID'] );	
 		}
-
 		return $orders;
 	}
 
-	protected function response( $msg, $orders = '' ){
+	protected function response( $msg, $orders = '' ) {
 		$response['message'] = $msg;
 		header( 'content-type: application/json; charset=utf-8' );
 		echo json_encode( $orders ) . '\n';
